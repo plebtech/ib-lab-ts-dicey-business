39a310f8f2b7ba999886f36bb630b0ffb931f33a
plebtech <nbmc@protonmail.com>
2020-03-23 14:52:41 -0500

    roughly halfway through porting to typescript.

--- dice.js
+++ dice.js
@@ -1,10 +1,102 @@
 console.log("Loading..");
-var CONTAINER = document.getElementById("container"), GENERATE_BTN = document.getElementById("controls").querySelectorAll("button")[0], REROLL_BTN = document.getElementById("controls").querySelectorAll("button")[0], SUM_BTN = document.getElementById("controls").querySelectorAll("button")[0], STATUS = document.getElementById("status");
-var counter = 1, dice = [];
-var testingTS = function () {
-    console.log(CONTAINER);
-    console.log(dice);
+var // global constant variables.
+CONTAINER = document.getElementById("container"), GENERATE_BTN = document.getElementById("controls").querySelectorAll("button")[0], REROLL_BTN = document.getElementById("controls").querySelectorAll("button")[0], SUM_BTN = document.getElementById("controls").querySelectorAll("button")[0], STATUS = document.getElementById("status"), STR_REMOVE = "<span>selected die removed.</span>";
+var // global mutable variables.
+counter = 1, // starts at 1, incremented AFTER die object created.
+dice = []; // empty array, dice pushed to it as they are created.
+var Die = /** @class */ (function () {
+    function Die() {
+        var _this = this;
+        // 'rolls' dice for one second before settling on final value.
+        this.animate = function (limit) {
+            if (limit === void 0) { limit = 1000; }
+            var start = Date.now();
+            var timer = setInterval(function () {
+                var timePassed = Date.now() - start;
+                if (timePassed >= limit) {
+                    clearInterval(timer);
+                    return;
+                }
+                _this.roll();
+            }, 100);
+        };
+        // call function to generate random die value (1-6).
+        this.roll = function (min, max) {
+            if (min === void 0) { min = 1; }
+            if (max === void 0) { max = 6; }
+            var r = randomDieVal(min, max);
+        };
+        // listeners for each die (click, double/right).
+        this.listen = function () {
+            var rIndex = dice.indexOf(_this);
+            _this.div.addEventListener('click', function () {
+                _this.animate();
+                STATUS.innerHTML = "<span>selected die rerolled.</span>";
+            });
+            _this.div.addEventListener('dblclick', function () {
+                _this.div.remove();
+                dice.splice(rIndex, 1);
+                reorderDice();
+                STATUS.innerHTML = STR_REMOVE;
+            });
+            _this.div.addEventListener('contextmenu', function () {
+                _this.div.remove();
+                dice.splice(rIndex, 1);
+                reorderDice();
+                STATUS.innerHTML = STR_REMOVE;
+            });
+        };
+        this.div = document.createElement('div'); // create div via DOM.
+        this.div.classList.add('die'); // assign class to div.
+        this.div.id = counter.toString(); // assign id to div (based on counter value);
+        CONTAINER.appendChild(this.div);
+        this.animate();
+        counter++;
+        dice.push(this);
+        this.listen();
+    }
+    return Die;
+}());
+var setNoun = function () {
+    var n;
+    if (dice.length === 1) {
+        n = 'die';
+    }
+    else if (dice.length > 1) {
+        n = 'dice';
+    }
+    return n;
 };
-testingTS();
-console.log("HAHA");
-console.log("Loaded.");
+// return random value between 1 and 6 inclusive.
+// use of Math.floor(Math.random()) requires formula of (((max+1) - min) + min) because Math.random() generates [0, 1).
+var randomDieVal = function (min, max) {
+    var val = Math.floor(Math.random() * ((max + 1) - min) + min);
+    return val;
+};
+// return ascii code for die 'face' based on passed value.
+var setFace = function (value) {
+    switch (value) {
+        case 1:
+            return '\u2680';
+        case 2:
+            return '\u2681';
+        case 3:
+            return '\u2682';
+        case 4:
+            return '\u2683';
+        case 5:
+            return '\u2684';
+        case 6:
+            return '\u2685';
+    }
+};
+// when die removed, reassign div ids based on # of dice left, set counter to # of dice.
+var reorderDice = function () {
+    var x = 0;
+    dice.forEach(function (die) {
+        die.div.id = x.toString();
+        x++;
+    });
+    counter = dice.length;
+};
+console.log("Loaded."); // if you see this, no errors.
--- dice.ts
+++ dice.ts
@@ -1,23 +1,112 @@
 console.log("Loading..");
 
-const
+const // global constant variables.
     CONTAINER: HTMLElement = document.getElementById("container"),
     GENERATE_BTN: HTMLElement = document.getElementById("controls").querySelectorAll("button")[0],
     REROLL_BTN: HTMLElement = document.getElementById("controls").querySelectorAll("button")[0],
     SUM_BTN: HTMLElement = document.getElementById("controls").querySelectorAll("button")[0],
-    STATUS: HTMLElement = document.getElementById("status");
+    STATUS: HTMLElement = document.getElementById("status"),
+    STR_REMOVE: string = `<span>selected die removed.</span>`;
 
-let
-    counter: number = 1,
-    dice: number[] = [];
+let // global mutable variables.
+    counter: number = 1, // starts at 1, incremented AFTER die object created.
+    dice: Die[] = []; // empty array, dice pushed to it as they are created.
 
-const testingTS = () => {
-    console.log(CONTAINER);
-    console.log(dice);
+class Die {
+    div: HTMLElement;
+    id: number;
+    value: number;
+    constructor() {
+        this.div = document.createElement('div'); // create div via DOM.
+        this.div.classList.add('die'); // assign class to div.
+        this.div.id = counter.toString(); // assign id to div (based on counter value);
+        CONTAINER.appendChild(this.div);
+        this.animate();
+        counter++;
+        dice.push(this);
+        this.listen();
+    }
+    // 'rolls' dice for one second before settling on final value.
+    animate = (limit: number = 1000): void => {
+        let start: number = Date.now();
+        const timer = setInterval(() => {
+            let timePassed: number = Date.now() - start;
+            if (timePassed >= limit) {
+                clearInterval(timer);
+                return;
+            }
+            this.roll();
+        }, 100);
+    }
+    // call function to generate random die value (1-6).
+    roll = (min: number = 1, max: number = 6): void => {
+        let r: number = randomDieVal(min, max);
+    }
+    // listeners for each die (click, double/right).
+    listen = (): void => {
+        let rIndex: number = dice.indexOf(this);
+        this.div.addEventListener('click', (): void => {
+            this.animate();
+            STATUS.innerHTML = `<span>selected die rerolled.</span>`;
+        });
+        this.div.addEventListener('dblclick', (): void => {
+            this.div.remove();
+            dice.splice(rIndex, 1);
+            reorderDice();
+            STATUS.innerHTML = STR_REMOVE;
+        });
+        this.div.addEventListener('contextmenu', (): void => {
+            this.div.remove();
+            dice.splice(rIndex, 1);
+            reorderDice();
+            STATUS.innerHTML = STR_REMOVE;
+        })
+    }
 }
 
-testingTS();
+const setNoun = (): string => {
+    let n: string;
+    if (dice.length === 1) {
+        n = 'die';
+    } else if (dice.length > 1) {
+        n = 'dice';
+    }
+    return n;
+}
 
-console.log("HAHA");
+// return random value between 1 and 6 inclusive.
+// use of Math.floor(Math.random()) requires formula of (((max+1) - min) + min) because Math.random() generates [0, 1).
+const randomDieVal = (min: number, max: number): number => {
+    let val: number = Math.floor(Math.random() * ((max + 1) - min) + min);
+    return val;
+}
 
-console.log("Loaded.")
+// return ascii code for die 'face' based on passed value.
+const setFace = (value: number): string => {
+    switch (value) {
+        case 1:
+            return '\u2680';
+        case 2:
+            return '\u2681';
+        case 3:
+            return '\u2682';
+        case 4:
+            return '\u2683';
+        case 5:
+            return '\u2684';
+        case 6:
+            return '\u2685';
+    }
+}
+
+// when die removed, reassign div ids based on # of dice left, set counter to # of dice.
+const reorderDice = (): void => {
+    let x: number = 0;
+    dice.forEach(die => {
+        die.div.id = x.toString();
+        x++;
+    });
+    counter = dice.length;
+}
+
+console.log("Loaded."); // if you see this, no errors.
